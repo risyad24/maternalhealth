e3fb2c27dbe172ea0714040f9ddaa15165278585
Risyad Abiyyu Siregar <rsiregar@oucru.org>
2025-12-24 15:34:43 +0700

    adding files

---
+++ run_daily_pipeline.R
@@ -0,0 +1,5 @@
+# Entry point for Windows Task Scheduler
+
+setwd("C:/Users/user/OneDrive - Oxford University Clinical Research Unit/Data/ANC")   # ← CHANGE THIS
+
+source("code/99_run_pipeline.R")
---
+++ run_spheres_pipeline.bat
@@ -0,0 +1,4 @@
+@echo off
+
+"C:\Users\user\AppData\Local\Programs\R\R-4.5.1\bin\x64\Rscript.exe" ^
+"C:\Users\user\OneDrive - Oxford University Clinical Research Unit\Data\ANC\run_daily_pipeline.R"
